041cc362b5da95d0d1f26e2ac25b67dcaf103f81
TylerLadas <tylerladas@hotmail.com>
2021-06-29 11:02:23 -0400

    add pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Global variables
+
+// Array variable
+
+// Check local storage for saved array items, if present array = localstorage if empty  array = []
+
+// Create local storage key to hold array items
+
+// Declare variable to hold parsed local storage data
+
+// Create function to push new values to array and then set to local storage
+
+// Function to dynamically create new local storage items (buttons?)
+
+// Loop through array (.foreach)
+
+// Clear storage button
+
+// Event listeners
