7e44ad1b8b340bbb93ab1549e2c8683f3065b139
TylerLadas <tylerladas@hotmail.com>
2021-06-30 13:48:36 -0400

    add array variable and create local storage key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 // Global variables
 
 // Array variable
-
+let searchArray;
 // Check local storage for saved array items, if present array = localstorage if empty  array = []
+if (localStorage.getItem('saved')) {
+    searchArray = JSON.parse(localStorage.getItem('saved'));
+} else {
+    searchArray = [];
+}
 
 // Create local storage key to hold array items
+localStorage.setItem('saved', JSON.stringify(searchArray));
 
 // Declare variable to hold parsed local storage data
+const savedSearches = JSON.parse(localStorage.getItem('saved'));
 
 // Create function to push new values to array and then set to local storage
 
