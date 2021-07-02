db5efb9755d05fa9e7233c5d73cf8f2d777262a4
TylerLadas <tylerladas@hotmail.com>
2021-07-02 10:03:58 -0400

    initial local storage code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 // Array variable
 let searchArray;
+
 // Check local storage for saved array items, if present array = localstorage if empty  array = []
 if (localStorage.getItem('saved')) {
     searchArray = JSON.parse(localStorage.getItem('saved'));
@@ -16,11 +17,45 @@ localStorage.setItem('saved', JSON.stringify(searchArray));
 const savedSearches = JSON.parse(localStorage.getItem('saved'));
 
 // Create function to push new values to array and then set to local storage
+//Confirm variable names to extract value from
+function setStorage() {
+    searchArray.push(search.value) 
+    localStorage.setItem('saved', JSON.stringify(searchArray))   
+};
+
+// Handler function for click events
+function handler(event) {
+    event.preventDefault;
+    // add location values
+    // run fetch function
+}
 
 // Function to dynamically create new local storage items (buttons?)
+function createButton(text) {
+let savedBtn = document.createElement("button");
+savedBtn.textContent = text
+savedBtn.className = "savedBtn";
+savedBtn.setAttribute("type", "submit")
+savedBtn.setAttribute("value", text);
+savedBtn.addEventListener("click", handler)
+savedContainer.appendChild(savedBtn);
+};
 
-// Loop through array (.foreach)
-
-// Clear storage button
+// Loop through array on pag load and render saved buttons
+searchArray.forEach(function(item) {
+    createButton(item)
+}); 
 
 // Event listeners
+
+// submit/search button listener
+
+
+// saved button listener
+
+
+// clear search button listener
+createButton.addEventListener('click', function() {
+    localStorage.clear
+    // insert code to remove html from saved container
+})
