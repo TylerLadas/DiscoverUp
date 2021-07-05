c36fd726a7f568bf0e4d67e03f8f0bed86f840da
Dennis de Guzman <84108180+d3guzman-dennis@users.noreply.github.com>
2021-07-04 21:01:24 -0400

    prefinal

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ let cityValue;
 //Execute search api for Events & Restos
 function lookUp(city) {
 
-var apiResto = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=';
+
 var apiEvent = 'https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json?size=5&';
 var eApiKey  = 'LVPi9sXqgdQ58cdxQJV9G5220uffRerh';
 
@@ -31,9 +31,26 @@ fetch(apiEvent + 'city=' + city + '&apikey=' + eApiKey)
                 // Initial response values stored into variables
                 var eId = item.id;
                 // Pick first image to display
-                //var eImageArr = JSON.parse(item.images);
                 var eImage = item.images[0].url;
+
+                //Location
+                var eVenue = item._embedded.venues[0].name;
+                var eAddress = item._embedded.venues[0].address.line1 + ', ' + 
+                               item._embedded.venues[0].city.name + ', ' +
+                               item._embedded.venues[0].state.stateCode + ', ' +
+                               item._embedded.venues[0].postalCode;
+
+                //Pull coordinates: longitude
+                var eLong = item._embedded.venues[0].location.longitude;
+                console.log(eLong);
                 
+                //Pull coordinates: latitude
+                var eLat = item._embedded.venues[0].location.latitude;
+                console.log(eLat);
+                
+                //Link to booking website
+                var eUrl = item.url;
+                //Event name
                 var eName = item.name;
                 //Store date [Option to use Moment to format]
                 var eDate = moment(item.dates.start.localDate).format('MMM Do, YYYY');
@@ -50,85 +67,82 @@ fetch(apiEvent + 'city=' + city + '&apikey=' + eApiKey)
                   eImage + '"style="width:200px;height:150px;"><br>Name: ' +
                   eName + ' On: ' +
                   eDate + ' at ' + 
-                  eTime + '<br>' +                  
-                  'Description: ' + eInfo +'<button id="a' +
-                  eId + '">More Info</button><br></br></div>');
-                  var aId ="a"+eId;
-                  if (aId.onclick) {
-                     $(eid).append('<div>Hello trial</div>');
-                  }
-                
-                
-                //return fetch(
-                  //  'https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events/' +
-                    //eId + '.json?city=ottawa&apikey=LVPi9sXqgdQ58cdxQJV9G5220uffRerh');
-                //.then(function(dResponse) {
-                  //  return response.json();
-                //})
-
-
+                  eTime + '<br> Location: ' +
+                  eVenue + ' at ' +
+                  eAddress + '<br> Description: ' +
+                  eInfo + '<a href="' +
+                  eUrl +'"> More Info. </a> <button onclick="restoTrigger()" id="a' +
+                  eId + '">Find Nearby Establishments.</button><br></br></div>');
+       
           });
 
        
-        
+        console.log('latitude', eLat);
         } else {
         //if totalRes = 0
         $('#responseEvents').append('<div> No event listings in your area at this time. </div>');
         }
+        
+        
     });     
 
-};    
-// fetch(apiResto, {
-    
-//     headers: {
-//      'Authorization':'Bearer 6hxmQPOKdlDPYMmYwZG-1Pf3M3WRSDx8fWmaiFrtBOmsgBjLFAlrLjyGgO1hqdBJwixNHpGAUD7y8LXpb371w-zua6t8fkEeYS74i9cKj_UolOYtOHJyw5K7jgTdYHYx',
-//  }
-// })
-//     .then(function(response) {
+};  
 
-//         return response.json();
-//     })
-//     .then(function(response) {
-//         //Console logs results from API JSON
-//         console.log(response);
-//         var totalRes = response.total;
-//         console.log(totalRes);
-//         if (totalRes > 0 ){
-//             $.each(response.businesses, function(i, item) {
-//                 // Store each business's object in a variable
-//                 var id = item.id;
-//                 var alias = item.alias;
-//                 var phone = item.display_phone;
-//                 var image = item.image_url;
-//                 var name = item.name;
-//                 var rating = item.rating;
-//                 var reviewcount = item.review_count;
-//                 var address = item.location.address1;
-//                 var city = item.location.city;
-//                 var state = item.location.state;
-//                 var zipcode = item.location.zip_code;
-//                 var rUrl = item.url;
-//                 // Append our result into our page
-//                 $('#responseRestos').append('<div id="' +
-//                     id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + 
-//                     image + '" style="width:200px;height:150px;"> We found <b>' + 
-//                     name + '</b> (' + alias + ')<br>Business ID: ' + 
-//                     id + '<br> Located at: ' + 
-//                     address + ' ' + 
-//                     city + ', ' + 
-//                     state + ' ' + 
-//                     zipcode + '<br>The phone number for this business is: ' + 
-//                     phone + '<br>This business has a rating of ' + 
-//                     rating + ' with ' + 
-//                     reviewcount + ' reviews. <br> Website: <a href="' +
-//                     rUrl + '">Click here</a></div>');
-//           });
+function restoTrigger () {
+//'&latitude=' + lookUp(eLat) + '&longitude=' + lookUp(eLong) + '&radius=20000'
+var apiResto = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=ottawa';
+fetch(apiResto, {
+    
+    headers: {
+      'Authorization':'Bearer 6hxmQPOKdlDPYMmYwZG-1Pf3M3WRSDx8fWmaiFrtBOmsgBjLFAlrLjyGgO1hqdBJwixNHpGAUD7y8LXpb371w-zua6t8fkEeYS74i9cKj_UolOYtOHJyw5K7jgTdYHYx',
+  }
+ })
+     .then(function(response) {
+
+         return response.json();
+     })
+     .then(function(response) {
+         //Console logs results from API JSON
+         console.log('resto',response);
+         var totalRes = response.total;
+         console.log(totalRes);
+         if (totalRes > 0 ){
+             $.each(response.businesses, function(i, item) {
+                 // Store each business's object in a variable
+                 var id = item.id;
+                 var alias = item.alias;
+                 var phone = item.display_phone;
+                 var image = item.image_url;
+                 var name = item.name;
+                 var rating = item.rating;
+                 var reviewcount = item.review_count;
+                 var address = item.location.address1;
+                 var city = item.location.city;
+                 var state = item.location.state;
+                 var zipcode = item.location.zip_code;
+                 var rUrl = item.url;
+                 // Append our result into our page
+                 $('#responseRestos').append('<div id="' +
+                     id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + 
+                     image + '" style="width:200px;height:150px;"><b>' + 
+                     name + '</b> (' + alias + ')<br>Located at: ' + 
+                     address + ' ' + 
+                     city + ', ' + 
+                     state + ' ' + 
+                     zipcode + '<br>Phone : ' + 
+                     phone + '<br>Rating : ' + 
+                     rating + ' with ' + 
+                     reviewcount + ' reviews. <br> Website: <a href="' +
+                     rUrl + '">Click here</a></div>');
+           });
 
        
-//         // Grab the results from the API JSON return
-//     } 
+         // Grab the results from the API JSON return
+     } 
 
-    // });     
+ });   
+ 
+};
 
 // Global variables
 
@@ -207,3 +221,4 @@ clearBtn.addEventListener('click', function() {
     // insert code to remove html from saved container
     savedContainer.innerHTML = "";
 })
+//};
